d022010901cd6fed30dd098b6c13b447f2b14593
ChaseTheCoder <chasefsheaff@gmail.com>
2021-08-06 15:46:35 -0500

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,11 @@
-#!/usr/bin/env sh
-# abort on errors
-set -e
-# build
 npm run build
-# navigate into the build output directory
+
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:ChaseTheCoder/visit-chicago.git master:gh-pages
-cd -
+
+git push -f https://github.com/chasethecoder/visit-chicago/ master:gh-pages
+
+cd ..
